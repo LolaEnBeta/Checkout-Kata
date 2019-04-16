6e49b13eac0947a05674c3cb339b03992fe05267
LolaEnBeta <lolitaenbeta@gmail.com>
2019-04-16 15:38:48 +0200

    .

--- checkout_kata.py
+++ checkout_kata.py
@@ -1,12 +1,25 @@
+
+def asigna_el_valor_a_cada_item(checkout, price_list):
+    for item in checkout:
+        if item == "A":
+            price_list.append(50)
+        if item == "B":
+            price_list.append(30)
+        if item == "C":
+            price_list.append(20)
+        if item == "D":
+            price_list.append(15)
+            
 items = {"A": 50, "B": 30, "C": 20, "D": 15}
 checkout = list()
 
-print(items)
+print("These are our products:", items)
 
 n = 1
 while n > 0:
     select_item = input("Choose one item from the list (for exit press Enter): ")
     checkout.append(select_item)
+    
     if select_item == "":
         checkout.remove(select_item)
         n = 0
@@ -18,15 +31,8 @@ while n > 0:
 print("You choose this these items:", checkout)
 
 price_list = list()
-for item in checkout:
-    if item == "A":
-        price_list.append(50)
-    if item == "B":
-        price_list.append(30)
-    if item == "C":
-        price_list.append(20)
-    if item == "D":
-        price_list.append(15)
+
+asigna_el_valor_a_cada_item(checkout, price_list)
 
 total_price = sum(price_list)
 print ("Total to pay:", total_price)
