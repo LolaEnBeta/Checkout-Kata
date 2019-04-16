35b2aa3b9a6e4ad4dd7717f1bc661d4804d4de16
LolaEnBeta <lolitaenbeta@gmail.com>
2019-04-16 12:36:14 +0200

    Remove items that not appear in items dict.

--- checkout_kata.py
+++ checkout_kata.py
@@ -6,9 +6,15 @@ print(items)
 n = 1
 while n > 0:
     select_item = input("Choose one item from the list (for exit press Enter): ")
-    if select_item == "":
-        n = 0
     checkout.append(select_item)
-    if "" in checkout:
-        checkout.remove("")
-print(checkout)
+    if select_item == "":
+        checkout.remove(select_item)
+        n = 0
+        break
+    if select_item not in items:
+        print("We don't have this yet, sorry!")
+        checkout.remove(select_item)
+    
+
+        
+print("You choose this these items:", checkout)
