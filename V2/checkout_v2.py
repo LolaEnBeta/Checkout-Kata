1f10c4480936b88727f2eb6a89ccb3debab35118
LolaEnBeta <lolitaenbeta@gmail.com>
2019-04-24 23:22:00 +0200

    Add offers to all products

--- V2/checkout_v2.py
+++ V2/checkout_v2.py
@@ -16,9 +16,19 @@ while selected_product != "":
     selected_product = input ("Select one item from de list: ")
 
 count_A = 0
+count_B = 0
+count_C = 0
+count_D = 0
 for product in selected_products:
     if product == "A":
         count_A += 1
+    elif product == "B":
+        count_B += 1
+    elif product == "C":
+        count_C += 1
+    elif product == "D":
+        count_D += 1
+
 
 total_price = 0
 while count_A > 0:
@@ -28,5 +38,13 @@ while count_A > 0:
     else:
         total_price += count_A * 50
         count_A = 0
+while count_B > 0:
+    if count_B >= 2:
+        total_price += 45
+        count_B -= 2
+    else:
+        total_price += count_B * 30
+        count_B = 0
+total_price = (count_C * 20) + (count_D * 15)
 
 print(total_price)  
