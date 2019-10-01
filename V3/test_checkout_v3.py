9da0e1587d9413a6b79206bd4cf273f9fddc74d7
LolaEnBeta <lolitaenbeta@gmail.com>
2019-10-01 15:41:36 +0200

    Pass the test with the function from checkout_v3

--- V3/test_checkout_v3.py
+++ V3/test_checkout_v3.py
@@ -1,11 +1,11 @@
 import unittest
+from checkout_v3 import sum_checkout
 
 class TestCheckout(unittest.TestCase):
 
     def test_sum_checkout(self):
         items = [50, 30]
-        total_price = (items[0] + items[1])
-        self.assertEqual(total_price, 80)
+        self.assertEqual(sum_checkout(items), 80)
 
 if __name__ == '__main__':
     unittest.main()
