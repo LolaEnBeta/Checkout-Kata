04c0308df8da9154e1820b8d5ae91814374fbb42
LolaEnBeta <lolitaenbeta@gmail.com>
2019-10-01 15:52:28 +0200

    Create a function to sum items from dictionary

--- V3/checkout_v3.py
+++ V3/checkout_v3.py
@@ -10,3 +10,7 @@ def sum_checkout(items):
     for item in items:
         total_price += item
     return total_price
+
+def sum_checkout_items_from_dictionary(items):
+    total_price = items["A"] + items["B"]
+    return total_price
