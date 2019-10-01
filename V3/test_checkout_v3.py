0534f1f450238a7eea4134c0eb0425d823b2aef2
LolaEnBeta <lolitaenbeta@gmail.com>
2019-10-01 15:54:10 +0200

    Pass test with dictionary function

--- V3/test_checkout_v3.py
+++ V3/test_checkout_v3.py
@@ -1,5 +1,5 @@
 import unittest
-from checkout_v3 import sum_checkout
+from checkout_v3 import sum_checkout, sum_checkout_items_from_dictionary
 
 class TestCheckout(unittest.TestCase):
 
@@ -9,8 +9,7 @@ class TestCheckout(unittest.TestCase):
 
     def test_sum_checkout_items_dictionary(self):
         items = {"A": 50, "B": 30}
-        total_price = items["A"] + items["B"]
-        self.assertEqual(total_price, 80)
+        self.assertEqual(sum_checkout_items_from_dictionary(items), 80)
 
 if __name__ == '__main__':
     unittest.main()
