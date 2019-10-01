5c9ea352a767a04bd54a16b5eec056bdd7efde92
LolaEnBeta <lolitaenbeta@gmail.com>
2019-10-01 15:46:19 +0200

    Rename the test function

--- V3/test_checkout_v3.py
+++ V3/test_checkout_v3.py
@@ -3,7 +3,7 @@ from checkout_v3 import sum_checkout
 
 class TestCheckout(unittest.TestCase):
 
-    def test_sum_checkout(self):
+    def test_sum_checkout_items_list(self):
         items = [50, 30]
         self.assertEqual(sum_checkout(items), 80)
 
