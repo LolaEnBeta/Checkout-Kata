13539b5e43c296c2b361ee033c7f6ea21799884e
LolaEnBeta <lolitaenbeta@gmail.com>
2019-10-01 15:34:17 +0200

    Make the test pass

--- V3/test_checkout_v3.py
+++ V3/test_checkout_v3.py
@@ -3,7 +3,9 @@ import unittest
 class TestCheckout(unittest.TestCase):
 
     def test_sum_checkout(self):
-        pass
+        items = [50, 30]
+        total_price = (items[0] + items[1])
+        self.assertEqual(total_price, 80)
 
 if __name__ == '__main__':
     unittest.main()
