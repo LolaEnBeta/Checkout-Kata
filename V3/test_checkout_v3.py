d78ba1700f24e9e0c30b2cb2a78127055f3fe121
LolaEnBeta <lolitaenbeta@gmail.com>
2019-10-02 16:04:31 +0200

    Create a test to add an item with the Cart method

--- V3/test_checkout_v3.py
+++ V3/test_checkout_v3.py
@@ -1,5 +1,6 @@
 import unittest
 from checkout_v3 import sum_checkout, sum_checkout_items_from_dictionary
+from Cart import Cart
 
 class TestCheckout(unittest.TestCase):
 
@@ -10,6 +11,11 @@ class TestCheckout(unittest.TestCase):
     def test_sum_checkout_items_dictionary(self):
         items = {"A": 50, "B": 30}
         self.assertEqual(sum_checkout_items_from_dictionary(items), 80)
+
+    def test_add_item(self):
+        new_cart = Cart()
+        new_cart.add_to_items_list("A")
+        self.assertEqual(new_cart.list, ["A"])
 
 if __name__ == '__main__':
     unittest.main()
