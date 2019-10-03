569dd9344ea709c12347afa6471153e1b22fe7c5
LolaEnBeta <lolitaenbeta@gmail.com>
2019-10-03 11:56:59 +0200

    Initialize the program code

--- V3/checkout_v3.py
+++ V3/checkout_v3.py
@@ -3,14 +3,6 @@
 # C = 20
 # D = 15
 
-items = [50, 30]
+prices = {"A": 50, "B": 30, "C": 20, "D": 15}
 
-def sum_checkout(items):
-    total_price = 0
-    for item in items:
-        total_price += item
-    return total_price
-
-def sum_checkout_items_from_dictionary(items):
-    total_price = items["A"] + items["B"]
-    return total_price
+cart = Cart()
