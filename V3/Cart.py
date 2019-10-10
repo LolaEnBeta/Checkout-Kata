e5a2f76eeb7c7953716e1849970eb900b9bd73df
LolaEnBeta <lolitaenbeta@gmail.com>
2019-10-10 18:33:37 +0200

    Refactor methods removing "return"

--- V3/Cart.py
+++ V3/Cart.py
@@ -17,7 +17,6 @@ class Cart():
                 self.total_items[item] += 1
             else:
                 self.total_items[item] = 1
-        return self.total_items
 
     def check_offers(self, offers):
         total_items = self.total_items
@@ -26,7 +25,6 @@ class Cart():
                 while self.total_items[item] >= offers[item]["units"]:
                     self.total_price += offers[item]["price"]
                     self.total_items[item] -= offers[item]["units"]
-        return self.total_price
 
     def calculate_total_price(self, prices):
         for item in self.total_items:
